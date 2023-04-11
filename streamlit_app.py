953b46a0e30022b4a44d90127711cb94db954d4d
romymassaad <130417379+romymassaad@users.noreply.github.com>
2023-04-11 16:50:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,61 @@
-from collections import namedtuple
-import altair as alt
-import math
+import requests
+import json
+from datetime import datetime
 import pandas as pd
-import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+class FarmerDashboard:
+    def __init__(self, location):
+        self.location = location
+        self.weather_data = self.get_weather_data()
+        self.crop_data = self.get_crop_data()
+        self.expert_advice = self.get_expert_advice()
+        self.marketplace = self.get_marketplace()
+    
+    def get_weather_data(self):
+        api_key = 'YOUR_API_KEY_HERE'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={self.location}&appid={api_key}'
+        response = requests.get(url)
+        data = json.loads(response.text)
+        weather = {}
+        weather['temperature'] = round((data['main']['temp'] - 273.15) * 9/5 + 32, 2)
+        weather['humidity'] = data['main']['humidity']
+        weather['wind_speed'] = data['wind']['speed']
+        weather['precipitation'] = 0
+        if 'rain' in data:
+            weather['precipitation'] = data['rain']['1h']
+        if 'snow' in data:
+            weather['precipitation'] = data['snow']['1h']
+        return weather
+    
+    def get_crop_data(self):
+        # integrate with IoT sensors to retrieve crop data
+        # return data in a pandas DataFrame format
+        return pd.read_csv('crop_data.csv')
+    
+    def get_expert_advice(self):
+        # integrate with expert advice database
+        return ['Article 1', 'Article 2', 'Video 1', 'Webinar 1']
+    
+    def get_marketplace(self):
+        # integrate with marketplace database
+        return ['Buyer 1', 'Buyer 2', 'Buyer 3']
+    
+    def set_alert(self, weather_condition):
+        # integrate with alert system to set notification for weather condition
+        pass
+    
+    def get_optimal_planting_harvesting_time(self):
+        # integrate with data analytics to retrieve optimal planting and harvesting time
+        return 'Planting time: March 1st, Harvesting time: August 15th'
+    
+    def reduce_water_usage(self):
+        # integrate with gamification system to reward farmers for reducing water usage
+        pass
+    
+if __name__ == '__main__':
+    dashboard = FarmerDashboard('Bekaa Valley, Lebanon')
+    print(dashboard.weather_data)
+    print(dashboard.crop_data.head())
+    print(dashboard.expert_advice)
+    print(dashboard.marketplace)
+    print(dashboard.get_optimal_planting_harvesting_time())
